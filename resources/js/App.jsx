ab719524cef7d25d7d5b7f01613c40968c6c09d9
nio04 <nishatislam3108@gmail.com>
2024-10-31 04:13:27 +0600

    react: products can be render from backend

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-    return <h1>hello</h1>;
+    const [mobiles, setMobiles] = useState([]);
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/mobiles")
+            .then((res) => res.json())
+            .then((data) => setMobiles(data.data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Mobile collection</h1>
+            <ul>
+                {mobiles.map((mobile) => (
+                    <li key={mobile.id}>{mobile.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
