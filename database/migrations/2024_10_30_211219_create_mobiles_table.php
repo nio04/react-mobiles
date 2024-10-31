bc61b867a1ab0c4ca859354cf9c1e26e43f58e86
nio04 <nishatislam3108@gmail.com>
2024-10-31 17:55:37 +0600

    add: more mobile attributes & updated factories & seeder according to it

--- database/migrations/2024_10_30_211219_create_mobiles_table.php
+++ database/migrations/2024_10_30_211219_create_mobiles_table.php
@@ -14,14 +14,17 @@ return new class extends Migration {
             $table->string('name');
             $table->string('brand');
             $table->string('price');
-            $table->string('type'); // official or unofficial
-            $table->string('battery');
+            $table->string('battery_capacity'); //capacity
+            $table->string('battery_type');
             $table->string('camera');
             $table->string('chipset');
+            $table->string('refresh_rate');
             $table->string('os');
             $table->string('ram');
             $table->string('storage');
+            $table->string('display_type');
             $table->string('status'); // available or unavailable
+            $table->string('network');
             $table->string("cover")->nullable();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Mobile;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder {
     /**
@@ -15,30 +16,35 @@ class DatabaseSeeder extends Seeder {
         $mobiles = [];
 
         for ($i = 1; $i <= 100000; $i++) {
+            $currentTimestamp = now();
+
             $mobiles[] = [
                 'name' => "Mobile " . $i,
                 'brand' => fake()->randomElement(
                     ['Samsung', 'Google', 'OnePlus', 'Xiaomi', 'Oppo', 'Vivo', 'Sony', 'Realme', 'Asus', 'Motorola',]
                 ),
                 'price' => fake()->numberBetween(10000, 100000),
-                'type' => fake()->randomElement(['official', 'unofficial']),
-                'battery' => fake()->numberBetween(2000, 6000) . ' mAh',
-                'camera' => fake()->randomElement(['12 MP', '16 MP', '48 MP', '64 MP']),
+                'battery_capacity' => fake()->numberBetween(4000, 7000) . ' mAh',
+                'battery_type' => fake()->randomElement(['Lithium Polymer (Li-Po)', 'Lithium-ion (Li-Ion)']),
+                'camera' => fake()->randomElement(['12 MP', '16 MP', '48 MP', '64 MP', '200 MP']),
                 'chipset' => fake()->randomElement(
                     ['Bionic', 'Exynos', 'Kirin', 'MediaTek', 'Qualcomm', 'Unisoc']
                 ),
+                'refresh_rate' => fake()->randomElement(['120Hz', '144Hz', '165Hz', '90Hz', '60Hz']),
                 'os' => fake()->randomElement(['Android', 'iOS', 'Windows', 'Java']),
                 'ram' => fake()->randomElement(['2 GB', '4 GB', '6 GB', '8 GB', '12 GB']),
                 'storage' => fake()->randomElement(['32 GB', '64 GB', '128 GB', '256 GB']),
-                'status' => fake()->randomElement(['available', 'unavailable']),
+                'display_type' => fake()->randomElement([' AMOLED', 'IPS LCD', 'LCD', 'OLED']),
+                'status' => fake()->randomElement(['official', 'unofficial', 'available', 'not available', 'upcoming']),
+                'network' => fake()->randomElement(['3G', '4G', '5G', 'WIFI', 'upcoming']),
                 'cover' => 'https://picsum.photos/seed/' . fake()->numberBetween(1, 1000) . '/400/300',
-
+                'created_at' => $currentTimestamp,
             ];
         }
 
         // Insert records in batches
-        foreach (array_chunk($mobiles, 5000) as $chunk) {
-            Mobile::insert($chunk);
+        foreach (array_chunk($mobiles, 1000) as $chunk) {
+            DB::table('mobiles')->insert($chunk);
         }
     }
 }
